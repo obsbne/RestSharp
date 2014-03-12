8bc104b2a90298a0c39d9257bc019abb0d9c770e
Matt Psaltis <matthew.psaltis@obs.com.au>
2014-03-12 12:38:55 +1000

    Added Assembly Signing

--- RestSharp/Properties/AssemblyInfo.cs
+++ RestSharp/Properties/AssemblyInfo.cs
@@ -15,4 +15,9 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d2b12a34-b748-47f9-8ad6-f84da992c64b")]
 
-[assembly: InternalsVisibleTo("RestSharp.IntegrationTests")]
+[assembly: InternalsVisibleTo("RestSharp.IntegrationTests, " +
+    "PublicKey=0024000004800000940000000602000000240000525341310004000001000100f916c8848f9ecd" +
+    "c4471ff9d4e8db19072e08ecdb9592cbb559896ad0121cf5a3a662c0cbd6f8fcd4326fc4dd4377" +
+    "d438a213a9ea3e85a314481410259eb718195b50ee319a0d8cd7095bacc46075a446f1a2daf4b2" +
+    "cf182415625cc113bfd0c96f4662c9936bab4b96f6b5ec716c723a4bd15b4b45d9825b379c1a54" +
+    "1c7847b5")]
